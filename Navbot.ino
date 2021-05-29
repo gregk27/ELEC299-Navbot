ef0df34f49e954005b03cea191721513681ff25a
Gregory Kelly <git@gregk.ca>
2021-05-29 16:45:14 -0400

    Added code to display position information

--- Navbot.ino
+++ Navbot.ino
@@ -1,9 +1,22 @@
+#include "./src/hardware/Drivetrain.h"
+#include "./src/hardware/Sensors.h"
+#include "./src/hardware/IMU.h"
+
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting");
+
+  Drivetrain::init(1, 3, 3, 2);
+  Sensors::init();
+  IMU::init();
 }
 
 void loop() {
-
+  IMU::Position pos = IMU::getPosition();
+  Serial.print(pos.x);
+  Serial.print("\t");
+  Serial.print(pos.y);
+  Serial.print("\t");
+  Serial.println(pos.heading);
 }

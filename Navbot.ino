59fad9a27fe8fcb4d5eb3ebd87e0d7dade4e7e31
Gregory Kelly <git@gregk.ca>
2021-06-09 22:27:57 -0400

    Cleaned up ino file

--- Navbot.ino
+++ Navbot.ino
@@ -2,7 +2,6 @@
 #include "./src/hardware/Sensors.h"
 #include "./src/hardware/IMU.h"
 #include "./src/utils/List.h"
-#include "./src/utils/MemoryFree.h"
 #include "./Scheduler.h"
 
 #include "./src/commands/DriveToPositionCommand.h"
@@ -14,68 +13,40 @@
 */
 List<IMU::Position> path = List<IMU::Position>(32);
 
+/**
+ * PID Controller shared by main navigation commands (saves alot of memory)
+*/
 PID_v2 pid1(0,0,0,PID::Direct);
-
-// DriveToPositionCommand driveOutCommand(0, 100, 200, 10, &pid1);
-// DriveToPositionCommand driveSidewaysCommand(-50, 100, 200, 10, &pid1);
-// DrivePathCommand driveBackCommand(&path, true, 200, 15, &pid1);
-// DriveToPositionCommand driveToStopCommand(0, 0, 170, 5, &pid1);
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting");
 
-  // Serial.println("Sizes");
-  // Serial.print("Pointer: ");
-  // Serial.println(sizeof(void*));
-  // Serial.print("DriveToPositionCommand: ");
-  // Serial.println(sizeof(DriveToPositionCommand));
-  // Serial.print("TurnToHeadingCommand: ");
-  // Serial.println(sizeof(TurnToHeadingCommand));
-  // Serial.print("ComputePathCommand: ");
-  // Serial.println(sizeof(ComputePathCommand));
-  // Serial.print("Scheduler: ");
-  // Serial.println(sizeof(Scheduler));
-
+  //
   Drivetrain::init(4, 3, 3, 2);
   Drivetrain::setOutput(0,0);
   Sensors::init();
 
+  // Create Scheduler
   Scheduler::master = new Scheduler(32);
-  // Scheduler::master->addCommand(&driveOutCommand);
-  // Scheduler::master->addDelay(1000);
-  // Scheduler::master->addCommand(&driveSidewaysCommand);
-  // Scheduler::master->addDelay(1000);
-  // Scheduler::master->addCommand(&driveBackCommand);
-  // Scheduler::master->addDelay(1000);
-  // Scheduler::master->addCommand(&driveToStopCommand);
 
+  // Schedule main navigation commands
   Scheduler::master->addCommand(new DriveToPositionCommand(0, 100, 220, 20, &pid1, &path));
   Scheduler::master->addCommand(new DriveToPositionCommand(-50, 125, 200, 10, &pid1, &path));
   Scheduler::master->addCommand(new DriveToPositionCommand(50, 175, 200, 10, &pid1, &path));
-  // Scheduler::master->addCommand(new DriveToPositionCommand(50, 100, 200, 10, &pid1));
-  // Scheduler::master->addCommand(new DriveToPositionCommand(0, 0, 200, 10, &pid1));
   Scheduler::master->addCommand(new DrivePathCommand(&path, true, 200, 15, &pid1));
-
-  // Scheduler::master->addCommand(new TurnToHeadingCommand(PI/2, false, 220, 0.01, 3000));
-  // Scheduler::master->addDelay(1000);
-  // Scheduler::master->addCommand(new TurnToHeadingCommand(-PI, false, 220, 0.01, 3000));
-  // Scheduler::master->addDelay(1000);
-  // Scheduler::master->addCommand(new TurnToHeadingCommand(PI, true, 220, 0.01, 3000));
-
+ 
+ 
   // Run selftest
-  Drivetrain::resetPosition();
+  while(selfTest()){};
 
-  IMU::init();
-
-
+  // Final setup
   delay(1000);
-  
+  Drivetrain::resetPosition();
+  IMU::init();
   Scheduler::master->init();
-  Serial.println(freeMemory());
 }
 
-float holdHeading;
 
 void loop() {
   // -------
@@ -85,19 +56,13 @@ void loop() {
   IMU::Position pos = IMU::getPosition();
   float usDist = Sensors::getUltrasonicDistance()->getSmoothed();
 
-  // Serial.print(Sensors::getLeftIR()->getLast());
-  // Serial.print("\t");
-  // Serial.print(Sensors::getRightIR()->getLast());
-  // Serial.print("\t");
-  // Serial.println(Gyro::getYaw()->getSmoothed());
-
-
   // -----
   //  Act
   // -----
-  // Drivetrain::setOutput(220,220);
+  // Run scheduler
   Scheduler::master->periodic();
   // IMU::toPlot();
+
   // Hang when done
   if(Scheduler::master->isFinished()){
     Serial.println("Done");

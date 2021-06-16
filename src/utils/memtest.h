2bcbe107c26c9e4dddc7e02204fe8ab9bfa08913
Gregory Kelly <git@gregk.ca>
2021-06-16 15:50:17 -0400

    Removed memtest

--- src/utils/memtest.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEMTEST_H
-#define MEMTEST_H
-
-// Memory testing function from https://forum.arduino.cc/t/measuring-free-memory-weird-results/43994/5
-
-unsigned int memtest(unsigned short int step=1);
-
-#endif
